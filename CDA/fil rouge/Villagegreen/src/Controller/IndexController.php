889a1b4dcf5931eb78ca996057469e1c5a710f8c
Samir <samirb80440@gmail.com>
2025-02-10 08:55:25 +0100

    controller

--- CDA/fil rouge/Villagegreen/src/Controller/IndexController.php
+++ CDA/fil rouge/Villagegreen/src/Controller/IndexController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AccueilController extends AbstractController
+final class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_accueil')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
         ]);
     }
 }
