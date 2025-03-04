36056a33fadc6a5fd66e4acdbe4414cb7ce17b81
samir <samirb80440@gmail.com>
2025-03-04 14:47:13 +0100

    catalogue controller fait (vue à faire)

--- CDA/fil rouge/Villagegreen/src/Controller/IndexController.php
+++ CDA/fil rouge/Villagegreen/src/Controller/IndexController.php
@@ -2,17 +2,105 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
+use App\Repository\CategorieRepository;
+use App\Repository\SousCategorieRepository;
+use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    private $categorieRepo;
+    private $produitRepo;
+    private $sousCategorieRepo;
+
+    public function __construct(CategorieRepository $categorieRepo, ProduitRepository $produitRepo, SousCategorieRepository $sousCategorieRepo)
+    {
+        $this->categorieRepo = $categorieRepo;
+        $this->produitRepo = $produitRepo;
+        $this->sousCategorieRepo = $sousCategorieRepo;
+    }
+
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        $categories = $this->categorieRepo->findAll();
+        $produits = $this->produitRepo->findAll();
+
         return $this->render('catalogue/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'controller_name' => 'CatalogueController',
+            'categories' => $categories,
+            'produits' => $produits,
         ]);
     }
+
+
+    #[Route('/sous-categorie', name: 'app_sousCategorie')]
+    public function SousCategorie(): Response
+    {
+        $sous_categories = $this->sousCategorieRepo->findAll();
+
+        return $this->render('catalogue/sousCategories.html.twig', [
+            'sous_categories' => $sous_categories,
+        ]);
+    }
+
+    #[Route('/sous-categorie-{id}', name: 'app_selectSousCategorie', requirements: ['id' => '\d+'])]
+    public function SelectSousCategorie(int $id): Response
+    {
+            $categories = $this->categorieRepo->find($id);
+            $sous_categories = $this->sousCategorieRepo->findBy(['Categorie' => $categories]);
+
+
+            return $this->render('catalogue/sousCategories.html.twig',[
+                'sous_categories' => $sous_categories,
+
+            ]);
+    }
+
+    #[Route('/produits', name:'app_produits')]
+    public function Produit(): Response
+    {
+        $produits = $this->produitRepo->findAll();
+
+        return $this->render('catalogue/produits.html.twig', [
+            'produits' => $produits,
+        
+    ]);
+     
+
+    }
+   
+   
+
+
+    #[Route('/produits-{id}', name:'app_selectproduit', requirements: ['id' => '\d+'])]
+    public function SelectProduit(int $id ): Response
+    {
+        $produits = $this->produitRepo->find($id);
+       
+
+        return $this->render('catalogue/produits.html.twig', [
+            'produits' => $produits,
+        
+    ]);
+    
+    }
+
+    #[Route('/produits-{id}', name:'app_detailproduit', requirements: ['id' => '\d+'])]
+    public function DetailProduit(int $id ): Response
+    {
+        $produits = $this->produitRepo->find($id);
+       
+
+        return $this->render('catalogue/detailproduits.html.twig', [
+            'produits' => $produits,
+        
+    ]);
+
+    }
+
+
 }
