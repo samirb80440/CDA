e99207900e72b051b3dee74bd81425f4b6047835
samir <samirb80440@gmail.com>
2025-03-04 13:01:27 +0100

    petit modif

--- CDA/fil rouge/Villagegreen/src/Controller/IndexController.php
+++ CDA/fil rouge/Villagegreen/src/Controller/IndexController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class IndexController extends AbstractController
+class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
+        return $this->render('catalogue/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
     }
